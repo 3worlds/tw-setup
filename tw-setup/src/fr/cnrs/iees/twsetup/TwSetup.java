fdefde8eb9db82fc5e2513cf0b382020ff825530
Ian Davies <ian.davies@anu.edu.au>
2020-08-21 04:58:51 +1000

    fiddle

--- tw-setup/src/fr/cnrs/iees/twsetup/TwSetup.java
+++ tw-setup/src/fr/cnrs/iees/twsetup/TwSetup.java
@@ -45,67 +45,52 @@ import au.edu.anu.twcore.project.ProjectPaths;
 import au.edu.anu.twcore.project.TwPaths;
 import fr.ens.biologie.generic.utils.Logging;
 
-
-
 /**
- * A class to properly generate the .3w repo with the necessary dependencies from the 3Worlds 
- * eclipse project.
+ * A class to properly generate the .3w repo with the necessary dependencies
+ * from the 3Worlds eclipse project.
  * 
- * CAUTION: before running this class, it is strongly recommended to completely clean the
- * ivy cache by removing the local and cache subdirectories and regenerating them
- * using the 3w libraries build.xml scripts in the proper order (ie omhtk > omugi > qgraph > aot
- * > tw-core > tw-apps > tw-uifx)
+ * CAUTION: before running this class, it is strongly recommended to completely
+ * clean the ivy cache by removing the local and cache subdirectories and
+ * regenerating them using the 3w libraries build.xml scripts in the proper
+ * order (ie omhtk > omugi > qgraph > aot > tw-core > tw-apps > tw-uifx)
  * 
  * @author Ian Davies
  * @date 10 Dec. 2017
  */
 public class TwSetup implements ProjectPaths, TwPaths {
-	
+
 	// please only increment these numbers (with caution). Never decrement.
 	public static final String VERSION_MAJOR = "0";
 	public static final String VERSION_MINOR = "0";
 //	public static final String VERSION_MICRO = "1";// IDD: Thurs 10/08/2020. sent to Sam Banks 
 	public static final String VERSION_MICRO = "2";
-	
+
 	public static final String MODELRUNNER_JAR = "modelRunner.jar";
 	public static final String MODELMAKER_JAR = "modelMaker.jar";
-	// this is supposed to return the root dir of all 3worlds libraries, e.g. /home/gignoux/git
+	// this is supposed to return the root dir of all 3worlds libraries, e.g.
+	// /home/gignoux/git
 	public static final String CODEROOT = Path.of(System.getProperty("user.dir")).getParent().getParent().toString();
-	// NB these two names cannot be extracted from the classes because the classes are
+	// NB these two names cannot be extracted from the classes because the classes
+	// are
 	// in tw-uifx.
 	private static final String MODELMAKER_CLASS = "au.edu.anu.twuifx.mm.MMmain";
 	private static final String MODELRUNNER_CLASS = "au.edu.anu.twuifx.mr.MRmain";
-	
+
 	private static File buildTwApplicationIvyFile() {
-		String ivyFile = "<ivy-module version=\"2.0\"\n" + 
-				"		xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n" + 
-				"		xmlns:m=\"http://maven.apache.org/POM/4.0.0\"\n" + 
-				"		xsi:noNamespaceSchemaLocation=\"http://ant.apache.org/ivy/schemas/ivy.xsd\">\n" + 
-				"\n" + 
-				"	<info	organisation=\"fr.cnrs.iees.tw-setup\"\n" + 
-				"			module=\"tw-setup\"\n" + 
-				"			revision=\"0.0.1\"\n" + 
-				"			status=\"integration\">\n" + 
-				"		<license name=\"gpl3\" url=\"https://www.gnu.org/licenses/gpl-3.0.txt\"/>\n" + 
-				"		<description>This module brings together all components required for 3Worlds including archetype</description>\n" + 
-				"	</info>\n" + 
-				"\n" + 
-				"	<configurations>\n" + 
-				"		<conf name=\"java library\"/>\n" + 
-				"	</configurations>\n" + 
-				"\n" + 
-				"	<publications>\n" + 
-				"		<artifact type=\"jar\" ext=\"jar\">\n" + 
-				"			<conf name=\"java library\"/>\n" + 
-				"		</artifact>\n" + 
-				"	</publications>\n" + 
-				"\n" + 
-				"	<dependencies>\n" + 
-				"		<dependency org=\"au.edu.anu.tw-uifx\" name=\"tw-uifx\" rev=\"[0.3.11,)\"/>\n" + 
-				"	</dependencies>\n" + 
-				"\n" + 
-				"</ivy-module>\n" + 
-				"";
+		String ivyFile = "<ivy-module version=\"2.0\"\n"
+				+ "		xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n"
+				+ "		xmlns:m=\"http://maven.apache.org/POM/4.0.0\"\n"
+				+ "		xsi:noNamespaceSchemaLocation=\"http://ant.apache.org/ivy/schemas/ivy.xsd\">\n" + "\n"
+				+ "	<info	organisation=\"fr.cnrs.iees.tw-setup\"\n" + "			module=\"tw-setup\"\n"
+				+ "			revision=\"0.0.1\"\n" + "			status=\"integration\">\n"
+				+ "		<license name=\"gpl3\" url=\"https://www.gnu.org/licenses/gpl-3.0.txt\"/>\n"
+				+ "		<description>This module brings together all components required for 3Worlds including archetype</description>\n"
+				+ "	</info>\n" + "\n" + "	<configurations>\n" + "		<conf name=\"java library\"/>\n"
+				+ "	</configurations>\n" + "\n" + "	<publications>\n" + "		<artifact type=\"jar\" ext=\"jar\">\n"
+				+ "			<conf name=\"java library\"/>\n" + "		</artifact>\n" + "	</publications>\n" + "\n"
+				+ "	<dependencies>\n"
+				+ "		<dependency org=\"au.edu.anu.tw-uifx\" name=\"tw-uifx\" rev=\"[0.3.11,)\"/>\n"
+				+ "	</dependencies>\n" + "\n" + "</ivy-module>\n" + "";
 		File outf = new File("bidon.xml");
 		PrintWriter writer;
 		try {
@@ -117,11 +102,11 @@ public class TwSetup implements ProjectPaths, TwPaths {
 		}
 		return outf;
 	}
-	
-	
+
 	/**
-	 * <p>For end users: generates a Jar with ModelMaker ready to run.
-	 * This jar is just a manifest with:
+	 * <p>
+	 * For end users: generates a Jar with ModelMaker ready to run. This jar is just
+	 * a manifest with:
 	 * <ul>
 	 * <li>tw-dep.jar</li>
 	 * </ul>
@@ -129,21 +114,21 @@ public class TwSetup implements ProjectPaths, TwPaths {
 	 * </p>
 	 */
 	public static void packModelMaker() {
-		ThreeWorldsJar packer = new ThreeWorldsJar(VERSION_MAJOR,VERSION_MINOR,VERSION_MICRO);
+		ThreeWorldsJar packer = new ThreeWorldsJar(VERSION_MAJOR, VERSION_MINOR, VERSION_MICRO);
 		String filename = MODELMAKER_JAR;
-		System.out.print("Packing 3Worlds model maker into "+filename+"...");
+		System.out.print("Packing " + filename + "...");
 		packer.setMainClass(MODELMAKER_CLASS);
-		packer.addDependencyOnJar(TW+Jars.separator+TW_DEP_JAR);
+		packer.addDependencyOnJar(TW + Jars.separator + TW_DEP_JAR);
 		File outFile = jarFile(filename);
 		packer.saveJar(outFile);
 		outFile.setExecutable(true, false);
 		System.out.println("done");
 	}
-	
+
 	private static File jarFile(String filename) {
 //		File file = new File(USER_ROOT+File.separator+filename);
-		File file = new File(TW_ROOT+File.separator+filename);
-		if (file.exists()) 
+		File file = new File(TW_ROOT + File.separator + filename);
+		if (file.exists())
 			file.delete();
 		try {
 			file.createNewFile();
@@ -152,30 +137,31 @@ public class TwSetup implements ProjectPaths, TwPaths {
 		}
 		return file;
 	}
-	
+
 	/**
-	 * <p>For end users: generates a Jar with ModelMaker ready to run.
-	 * This jar is just a manifest with:
+	 * <p>
+	 * For end users: generates a Jar with ModelMaker ready to run. This jar is just
+	 * a manifest with:
 	 * <ul>
 	 * <li>tw-dep.jar</li>
-	 * </ul>//		for (String s:getProjectDependencies("tw-uifx")) 
-//			packer.addJar(s);
-
+	 * </ul>
+	 * // for (String s:getProjectDependencies("tw-uifx")) // packer.addJar(s);
+	 * 
 	 * assuming these two jars reside in the same directory as ModelMaker.jar
 	 * </p>
 	 */
 	public static void packModelRunner() {
-		ThreeWorldsJar packer = new ThreeWorldsJar(VERSION_MAJOR,VERSION_MINOR,VERSION_MICRO);
+		ThreeWorldsJar packer = new ThreeWorldsJar(VERSION_MAJOR, VERSION_MINOR, VERSION_MICRO);
 		String filename = MODELRUNNER_JAR;
-		System.out.println("Packing 3worlds model runner into "+filename+"...");
-		packer.addDependencyOnJar(TW+Jars.separator+TW_DEP_JAR);
+		System.out.println("Packing 3worlds model runner into " + filename + "...");
+		packer.addDependencyOnJar(TW + Jars.separator + TW_DEP_JAR);
 		packer.setMainClass(MODELRUNNER_CLASS);
 		File outFile = jarFile(filename);
 		packer.saveJar(outFile);
 		outFile.setExecutable(true, false);
 		System.out.println("done");
 	}
-	
+
 //	@SuppressWarnings("unused")
 //	private static Collection<String> getProjectDependencies(String projectName) {
 //		System.out.println("getting dependencies for library "+projectName);
@@ -187,31 +173,32 @@ public class TwSetup implements ProjectPaths, TwPaths {
 //		Collection<String> result = solver.getJars();
 //		return result;
 //	}
-	
+
 	/**
-	 * gets all the dependencies of 3worlds and pack them in a single jar.
-	 * Uses the ivy.xml file of each library to get the dependencies.
+	 * gets all the dependencies of 3worlds and pack them in a single jar. Uses the
+	 * ivy.xml file of each library to get the dependencies.
 	 * 
-	 * In the general use-case, 3w developers would probably send this jar file to end-users so
-	 * that they do not have to manage the library mess. Or ?
+	 * In the general use-case, 3w developers would probably send this jar file to
+	 * end-users so that they do not have to manage the library mess. Or ?
 	 */
 	public static void pack3wDependencies() {
-		ThreeWorldsJar packer = new ThreeWorldsJar(VERSION_MAJOR,VERSION_MINOR,VERSION_MICRO);
+		ThreeWorldsJar packer = new ThreeWorldsJar(VERSION_MAJOR, VERSION_MINOR, VERSION_MICRO);
 		String filename = TW_DEP_JAR;
-		System.out.println("Packing 3worlds dependencies into "+filename+":");
-		System.out.println("- assuming the 3Worlds code root directory is "+CODEROOT);
+		System.out.println("Packing 3worlds dependencies into " + filename + ":");
+		System.out.println("- assuming the 3Worlds code root directory is " + CODEROOT);
 		System.out.println("  (if this is wrong, edit the CODEROOT constant in TwSetup.java and re-run)");
 		// get all dependencies of all 3w libraries
 		// and pack them in a single jar
 		// this puts in everything since tw-uifx depends on all libraries
 		List<String> other = new ArrayList<>();
 		List<String> tw = new ArrayList<>();
-		for (String s: new DependencySolver(buildTwApplicationIvyFile().toString()).getJars()) {
+		for (String s : new DependencySolver(buildTwApplicationIvyFile().toString()).getJars()) {
 			packer.addJar(s);
+			String name = new File(s).getName();
 			if (s.contains("au.") || s.contains("fr."))
-				tw.add(new File(s).getName());
-			else
-				other.add(new File(s).getName());
+				tw.add(name);
+			else if (!other.contains(name))
+				other.add(name);
 		}
 		System.out.println("Packing...");
 		// except the code of tw-core. Why ??? Is this because we are in this project ?
@@ -228,19 +215,18 @@ public class TwSetup implements ProjectPaths, TwPaths {
 		File outFile = jarFile(filename);
 		packer.saveJar(outFile);
 		// no main class
-		// not executable	
+		// not executable
 		Collections.sort(other);
 		Collections.sort(tw);
 		System.out.println("------------- THIRD PARTY ---------------");
 		int count = 0;
-		for (String s:other)
-			System.out.println(++count + "\t"+s);
+		for (String s : other)
+			System.out.println(++count + "\t" + s);
 		System.out.println("------------- 3WORLDS LIBS --------------");
 		count = 0;
-		for (String s:tw)
-			System.out.println(++count+"\t"+s);
-		
-		
+		for (String s : tw)
+			System.out.println(++count + "\t" + s);
+
 		System.out.println("...done");
 	}
 
@@ -263,14 +249,16 @@ public class TwSetup implements ProjectPaths, TwPaths {
 //	}
 
 	/**
-	 * Creates the .3w directory and the dependency jar for 3Worlds if they do not exist
+	 * Creates the .3w directory and the dependency jar for 3Worlds if they do not
+	 * exist
+	 * 
 	 * @param args
 	 */
 //	public static void main(String... args) throws IOException { : NB String... can cause problems with finding main signature! Should be String[]
-		public static void main(String[] args) throws IOException {
-			Logging.setDefaultLogLevel(Level.OFF);
+	public static void main(String[] args) throws IOException {
+		Logging.setDefaultLogLevel(Level.OFF);
 		// 1) create /.3w
-		//       		|-->/lib
+		// |-->/lib
 		System.out.println("Setting up 3Worlds environment:");
 		System.out.println("Creating the .3w directory");
 //		File f = new File(USER_ROOT + File.separator + TW + File.separator +"lib");
@@ -285,8 +273,9 @@ public class TwSetup implements ProjectPaths, TwPaths {
 //		System.out.println("Skipping ModelMaker,jar");
 		packModelMaker();
 //		System.out.println("Skipping ModelMaker,jar");
-		//packModelRunner(); NO! Instead run the jar of the project <MyProject>.jar - tested 30/6/2020
-		FileUtilities.deleteFileTree( new File(DependencySolver.destPath));
+		// packModelRunner(); NO! Instead run the jar of the project <MyProject>.jar -
+		// tested 30/6/2020
+		FileUtilities.deleteFileTree(new File(DependencySolver.destPath));
 		System.out.println("FINISHED");
 		// 5) zip ModelMaker = .3w dir for distribution to end users
 		// do it from the system - it's easier !
