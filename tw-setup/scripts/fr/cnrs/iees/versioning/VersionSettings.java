ef1b39c1375f85dd8b4d4924fbca74f12366465c
Jacques Gignoux <jacques.gignoux@upmc.fr>
2020-11-30 15:57:57 +0100

    dependency consistency

--- tw-setup/scripts/fr/cnrs/iees/versioning/VersionSettings.java
+++ tw-setup/scripts/fr/cnrs/iees/versioning/VersionSettings.java
@@ -2,14 +2,14 @@
  *  TW-SETUP - tw-dep.jar generator                                       *
  *                                                                        *
  *  Copyright 2018: Shayne Flint, Jacques Gignoux & Ian D. Davies         *
- *       shayne.flint@anu.edu.au                                          * 
+ *       shayne.flint@anu.edu.au                                          *
  *       jacques.gignoux@upmc.fr                                          *
- *       ian.davies@anu.edu.au                                            * 
+ *       ian.davies@anu.edu.au                                            *
  *                                                                        *
  *  This module is specifically for generating the dependency file for    *
  *  3Worlds: tw-dep.jar.                                                  *
  *                                                                        *
- **************************************************************************                                       
+ **************************************************************************
  *  This file is part of TW-CORE (3Worlds Core).                          *
  *                                                                        *
  *  SETUP-CORE is free software: you can redistribute it and/or modify    *
@@ -20,7 +20,7 @@
  *  TW-SETUP is distributed in the hope that it will be useful,           *
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of        *
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *  GNU General Public License for more details.                          *                         
+ *  GNU General Public License for more details.                          *
  *                                                                        *
  *  You should have received a copy of the GNU General Public License     *
  *  along with TW-CORE.                                                   *
@@ -30,43 +30,43 @@
 package fr.cnrs.iees.versioning;
 
 public class VersionSettings {
-	
+
 	// Change these fields to suit the project ====================================================
-	
+
 	/** The organisation name as will appear in the ivy module specification - it is a good idea
 	 * to keep it consistent with the project src directory (although not required).*/
 	protected static String ORG = "fr.cnrs.iees.tw-setup";
-	
-	/** The name of the ivy module (this will be the name of the generated jar file for 
+
+	/** The name of the ivy module (this will be the name of the generated jar file for
 	 * dependent projects).*/
 	protected static String MODULE = "tw-setup";
-	
+
 	/** The ivy status of the module: integration, milestone, or release are the ivy defaults
 	 * But we can define ours like bronze, gold, silver, or crap, supercrap, ultracrap. */
 	protected static String STATUS = "integration";
-	
+
 	/** The license under which this module (= jar) is distributed */
 	protected static String LICENSE = "gpl3";
-	
+
 	/**The url to the text of the license */
 	protected static String LICENSE_URL = "https://www.gnu.org/licenses/gpl-3.0.txt";
-	
+
 	/**A (long) description of the ivy module */
-	protected static String DESCRIPTION = 
+	protected static String DESCRIPTION =
 		"This module is specifically for generating the dependency file for 3Worlds: tw-dep.jar.";
-	
+
 	/**
 	 * <p>Dependencies on other modules (they will be integrated in the ivy script).</p>
-	 * 
+	 *
 	 * <p>This is a (n * 4) table of Strings.<br/>
 	 * Every line is a new dependency.
 	 * On every line, the 4 Strings must match the ivy fields:
 	 * <dl>
-	 * <dt>org</dt> <dd>for <em>organisation</em></dd> 
+	 * <dt>org</dt> <dd>for <em>organisation</em></dd>
 	 * <dt>name</dt> <dd> for the module <em>name</em></dd>
 	 * <dt>rev</dt> <dd>for the <em>revision</em> or version number. The '+' can
 	 * be conveniently used to specify 'any version'.</dd>
-	 * <dt>m:classifier</dt> <dd>for the <em>type of artifact</em> within the module. It's actually a 
+	 * <dt>m:classifier</dt> <dd>for the <em>type of artifact</em> within the module. It's actually a
 	 * maven field, not an ivy filed. It enables to identify
 	 * different artifacts such as source, javadoc, linux or windows specific packagings. This field
 	 * is optional, put 'null' if not needed. Valid values are: <em>sources, javadoc, _os</em>.
@@ -74,23 +74,23 @@ public class VersionSettings {
 	 *  - to match the needs of javafx components. For other libraries, you should check in the maven
 	 * central repository the actual file names by clicking on the 'View All' button in the
 	 * top table, on the 'Files' table entry</em></dd>
-	 * </dl> 
+	 * </dl>
 	 * The field can be empty (just needs the external braces).<br/>
-	 * Example value: 
+	 * Example value:
 	 * <pre>{{"org.galaxy.jupiter","crap","1.0.+"},
 	 * {"org.ocean.lostIsland","strungk","3.12.254"}}</pre> </p>
 	 * <p>Wildcards for revision numbers are indicated <a href="http://ant.apache.org/ivy/history/master/ivyfile/dependency.html">there</a>.</p>
-	 * 
+	 *
 	 */
-	protected static String[][] DEPS = { 
+	protected static String[][] DEPS = {
 			{"fr.ens.biologie", "generics", "[0.2.2,)", null},
-			{"fr.cnrs.iees.tw-core", "tw-core", "[0.3.26,)", null},
+			{"fr.cnrs.iees.tw-core", "tw-core", "[0.3.27,)", null},
 			{"org.apache.ivy", "ivy", "2.2.0", null},
 	};
-	
+
 	/** The name of the main class to put in the jar manifest, if any. This enables users to
 	 * run the jar using this class as the entry point. Of course this must be a fully qualified
-	 * valid java class name found in the jar. 
+	 * valid java class name found in the jar.
 	 */
 	protected static String MAINCLASS = null;
 
