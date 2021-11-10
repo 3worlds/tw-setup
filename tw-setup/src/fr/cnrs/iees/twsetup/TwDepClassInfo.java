1fb8b48701ab6c7b5a2f6d3e8774c56238323873
Ian Davies <ian.davies@anu.edu.au>
2021-11-11 10:48:09 +1100

    count compressed bytes in tw-dep.jar

---
+++ tw-setup/src/fr/cnrs/iees/twsetup/TwDepClassInfo.java
@@ -0,0 +1,45 @@
+package fr.cnrs.iees.twsetup;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.NumberFormat;
+import java.util.Enumeration;
+import java.util.Locale;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+
+public class TwDepClassInfo {
+
+	public static void main(String[] args) {
+		String fn = System.getProperty("user.home") + "/.3w/tw-dep.jar";
+		File f = new File(fn);
+		long sumClassCompressed = 0;
+		long sumClassUncompressed = 0;
+		JarFile jf;
+		try {
+			// NB: Don't use jar input streams. These will not contain the info required!
+			// Not much use doing the same for .java because they are not all there so
+			// comparison with .class file means nothing.
+			jf = new JarFile(f.getAbsolutePath());
+			Enumeration e = jf.entries();
+			while (e.hasMoreElements()) {
+				JarEntry je = (JarEntry) e.nextElement();
+				String name = je.getName();
+				if (name.endsWith(".class")) {
+					sumClassCompressed += je.getCompressedSize();
+					sumClassUncompressed += je.getSize();
+				}
+			}
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		System.out.println(fn);
+		System.out.println(
+				"Compressed .class file bytes:\t" + NumberFormat.getNumberInstance().format(sumClassCompressed));
+		System.out.println(
+				"Uncompressed .class file bytes:\t" + NumberFormat.getNumberInstance().format(sumClassUncompressed));
+
+	}
+
+}
