ace3ac1be42eb16de6923c66080d6fba09a84c62
Jacques Gignoux <jacques.gignoux@upmc.fr>
2022-04-06 14:48:16 +0200

    updating license header in all files

--- tw-setup/scripts/fr/cnrs/iees/versioning/VersionManager.java
+++ tw-setup/scripts/fr/cnrs/iees/versioning/VersionManager.java
@@ -1,3 +1,32 @@
+/**************************************************************************
+ *  TW-SETUP - tw-dep.jar generator                                       *
+ *                                                                        *
+ *  Copyright 2018: Shayne Flint, Jacques Gignoux & Ian D. Davies         *
+ *       shayne.flint@anu.edu.au                                          * 
+ *       jacques.gignoux@upmc.fr                                          *
+ *       ian.davies@anu.edu.au                                            * 
+ *                                                                        *
+ *  This module is specifically for generating the dependency file for    *
+ *  3Worlds: tw-dep.jar.                                                  *
+ *                                                                        *
+ **************************************************************************                                       
+ *  This file is part of TW-SETUP.                                        *
+ *                                                                        *
+ *  TW-SETUP is free software: you can redistribute it and/or modify      *
+ *  it under the terms of the GNU General Public License as published by  *
+ *  the Free Software Foundation, either version 3 of the License, or     *
+ *  (at your option) any later version.                                   *
+ *                                                                        *
+ *  TW-SETUP is distributed in the hope that it will be useful,           *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *  GNU General Public License for more details.                          *                         
+ *                                                                        *
+ *  You should have received a copy of the GNU General Public License     *
+ *  along with TW-SETUP.                                                  *
+ *  If not, see <https://www.gnu.org/licenses/gpl.html>                   *
+ *                                                                        *
+ **************************************************************************/
 package fr.cnrs.iees.versioning;
 
 /**********************************************************************************************
